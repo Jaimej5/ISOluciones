36ed5c83858755bf2a3ee998a329f7898a4b3589
Jaimej5 <43786797+Jaimej5@users.noreply.github.com>
2018-11-23 01:54:02 +0100

    Subida github

---
+++ src/main/java/ISOlI/ISOluciones/App.java
@@ -0,0 +1,13 @@
+package ISOlI.ISOluciones;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
